958d332c5374538c4185a881d8a30db60ce00154
Prashanth <pyerramilli@godaddy.com>
2016-03-05 10:42:37 -0700

    changes to get items from a list

--- app/components/GroceryItemList.jsx
+++ app/components/GroceryItemList.jsx
@@ -3,7 +3,18 @@ var React = require('react/addons');
 module.exports = React.createClass({
     render : function(){
         return (
-          <div><h1>Grocery Listify</h1> </div>
+          <div>
+            <h1>Grocery Listify</h1>
+            <div>
+                  {
+                      this.props.items.map(function(item, index){
+                          return (
+                                <div>{item.name}</div>
+                          )
+                      })
+                  }
+            </div>
+          </div>
         )
     }
 });
--- app/main.jsx
+++ app/main.jsx
@@ -3,4 +3,20 @@ var React = require('react/addons');
 console.log('inside main jsx file');
 var GroceryItemList = require('./components/GroceryItemList.jsx');
 
-React.render(<GroceryItemList />, app);
+var initialItems = [
+    {
+        name: "Ice Cream"
+    },
+    {
+        name: "Waffles"
+    },
+    {
+        name: "Candy",
+        purchased : true
+    },
+    {
+        name: "Snarks"
+    },
+]
+
+React.render(<GroceryItemList items={initialItems}/>, app);
